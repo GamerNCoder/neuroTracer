6ae82e38b1140a6291b54e07c79b17503059de71
Arnav Rastogi <getarnavrastogi@gmail.com>
2026-05-10 21:27:23 -0400

    feat(pwa): Workbox offline shell, richer manifests, and docs

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,6 +1,17 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0f172a" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
